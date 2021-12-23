37d2ea17891d91615594ff762512ffd697add76d
manjugoutham <gouthamy219@gmail.com>
2021-12-23 11:05:43 +0530

    find frequency of words in a large paragraph

--- Hashtabletest.java
+++ Hashtabletest.java
@@ -6,7 +6,7 @@ public class Hashtabletest {
 
 		Hashtable<String, Integer> hashtable = new Hashtable<>();
 
-		String message = "To be or not to be";
+		String message = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
 		String[] value = message.toLowerCase().split(" ");
 
